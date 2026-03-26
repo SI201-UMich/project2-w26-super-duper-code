da5ddaaf8a769cf07a6d10a4775d3e0bace6da65
Mischa <mischa@MacBook-Air-2029.local>
2026-03-26 15:47:44 -0400

    Name/Intro

--- project2_starter.py
+++ project2_starter.py
@@ -1,7 +1,7 @@
 # SI 201 HW4 (Library Checkout System)
-# Your name: Nahida Sultana
-# Your student id: 11038752
-# Your email: nahidas@umich.edu
+# Your name: Nahida Sultana, Mischa Rafferty
+# Your student id: 11038752, 7593 8200
+# Your email: nahidas@umich.edu, msraff@umich.edu
 # Who or what you worked with on this homework (including generative AI like ChatGPT):
 # If you worked with generative AI also add a statement for how you used it.
 # e.g.:
